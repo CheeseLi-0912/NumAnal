2ce51aaeac183feea21292df2f6f08c8469e2814
CheeseLi-0912 <lipingchuan138@gmail.com>
2023-10-12 22:10:10 +0800

    new project

---
+++ Polynomial_Interpolation/Polynomial_Interpolation/A.h
@@ -0,0 +1,59 @@
+// Newton Formula
+#include <iostream>
+using namespace std;
+
+class Newton_Formula
+{
+private:
+
+	double* x; // interpolated value, carried by pointer
+	int n; // the number of interpolated value
+	double t; //given value, aiming to get f(t)
+
+public:
+
+	virtual double f(double x) = 0;
+
+	Newton_Formula(double* x, int n, double t);
+
+	double pai(int k);
+
+	double a(int i, int j);
+
+	double solve();
+};
+
+Newton_Formula::Newton_Formula(double* x, int n, double t)
+{
+	this->x = x;
+	this->n = n;
+	this->t = t;
+}
+
+double Newton_Formula::pai(int k)
+{
+	double res = 1;
+	for (int i = 0; i <= k - 1; i++)
+	{
+		res *= t - x[i];
+	}
+	return res;
+}
+
+double Newton_Formula::a(int i, int j)
+{
+	if (i == j)
+		return f(x[i]);
+	else
+		return (a(i + 1, j) - a(i, j - 1)) / (x[j] - x[i]);
+}
+
+double Newton_Formula::solve()
+{
+	double res = 0;
+	for (int k = 0; k <= n; k++)
+	{
+		res += a(0, k) * pai(k);
+	}
+	return res;
+}
---
+++ Polynomial_Interpolation/Polynomial_Interpolation/B.cpp
@@ -0,0 +1,37 @@
+#include "A.h"
+
+class B1 :public Newton_Formula
+{
+public:
+	virtual double f(double x)
+	{
+		int _x = x;
+		switch (_x)
+		{
+		case 0:
+			return 6;
+		case 1:
+			return -3;
+		case 2:
+			return -6;
+		case 3:
+			return 9;
+		}
+	}
+
+	B1(double* x, int n, double t) :Newton_Formula(x,n,t)
+	{}
+
+};
+
+int main()
+{
+	Newton_Formula* s;
+	double x[4] = { 0, 1, 2, 3 };
+	B1 b1(x, 4, 1.5);
+
+	s = &b1;
+	cout << s->solve() << endl;
+
+	return 0;
+}
