aa3061de9397adac6cceaf10703d38cf8e57ca1b
CheeseLi-0912 <lipingchuan138@gmail.com>
2023-10-13 09:44:44 +0800

    mo

--- Polynomial_Interpolation/Polynomial_Interpolation/B.cpp
+++ Polynomial_Interpolation/Polynomial_Interpolation/B.cpp
@@ -1,37 +1,35 @@
-#include "A.h"
-
-class B1 :public Newton_Formula
-{
-public:
-	virtual double f(double x)
-	{
-		int _x = x;
-		switch (_x)
-		{
-		case 0:
-			return 6;
-		case 1:
-			return -3;
-		case 2:
-			return -6;
-		case 3:
-			return 9;
-		}
-	}
-
-	B1(double* x, int n, double t) :Newton_Formula(x,n,t)
-	{}
-
-};
+//#include "A.h"
+#include <fstream>
+#include <string>
+#include <stdio.h>
+//class B :public Newton_Formula
+//{
+//public:
+//	virtual double f(double x)
+//	{
+//		return 1.0 / (1 + x * x);
+//	}
+//
+//	B(double* x, int n, double t) :Newton_Formula(x, n, t)
+//	{}
+//
+//};
 
 int main()
 {
-	Newton_Formula* s;
-	double x[4] = { 0, 1, 2, 3 };
-	B1 b1(x, 4, 1.5);
+	//Newton_Formula* s;
+	//double x[4] = { 0, 1, 2, 3 };
+	//B1 b1(x, 3, 1.5);
 
-	s = &b1;
-	cout << s->solve() << endl;
+	//s = &b1;
+	//cout << s->solve() << endl;
+	// 
+	//plot
+
+	FILE* pipe = _popen("gnuplot", "w"); /* 以可写方式打开gnuplot终端，如果pipe为空则说明打开失败，可能是因为未将gnuplot设置在PATH环境变量中 */
+	fprintf(pipe, "plot sin(x)\n"); /* 将gnuplot的画图命令输出到管道中，命令需要以换行符结束 */
+	fprintf(pipe, "pause mouse\n"); /* 使用此命令可以让画图窗口停留，作用为等待用户点击鼠标后关闭窗口 */
+	_pclose(pipe); /* 关闭pipe管道，gnuplot退出 */
 
 	return 0;
 }
